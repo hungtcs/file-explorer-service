b13857b22a8cc11a61c9bf89126dbf0d4590a952
刘文杰 <lwenjie@hirisun.com>
2019-08-14 23:42:23 +0800

    feat(file-traversal): add delete, copy, create functions

--- src/modules/file-traversal/file-traversal/file-traversal.controller.ts
+++ src/modules/file-traversal/file-traversal/file-traversal.controller.ts
@@ -1,9 +1,10 @@
-import { exists } from 'fs';
-import { resolve } from 'path';
+import { getType } from 'mime';
 import { Response } from 'express';
 import { promisify } from 'util';
 import { FileService } from '../services/public_api';
-import { Controller, Get, Param, Res, Query } from '@nestjs/common';
+import { resolve, basename } from 'path';
+import { exists, createReadStream, stat } from 'fs';
+import { Controller, Get, Param, Res, Query, HttpStatus, Delete, Put, Body } from '@nestjs/common';
 
 @Controller('file-traversal')
 export class FileTraversalController {
@@ -26,6 +27,58 @@ export class FileTraversalController {
       }));
     }
     response.json(fileCarte);
+  }
+
+  @Get('download/:path')
+  public async downloadFile(@Res() response: Response, @Param('path') path: string) {
+    if(!(await promisify(exists)(path))) {
+      response.status(HttpStatus.NOT_FOUND).json({ message: 'file not found' });
+      return;
+    }
+    const fileStat = await promisify(stat)(path);
+    if(fileStat.isDirectory()) {
+      response.status(HttpStatus.BAD_REQUEST).json({ message: 'path is a folder' });
+      return;
+    }
+    const readStream = createReadStream(path);
+    response.writeHead(HttpStatus.OK, {
+      'Content-Type': getType(path),
+      'Content-Length': fileStat.size,
+      'Content-Disposition': `attachment; filename="${ global.encodeURI(basename(path)) }"`,
+      'Content-Transfer-Encoding': 'binary',
+    });
+    readStream.pipe(response).on('end', () => response.end());
+  }
+
+  @Delete()
+  public async deleteFiles(@Query('files') files: string | Array<string>) {
+    if(files instanceof Array) {
+      return await this.fileService.deleteFiles(files);
+    } else {
+      return await this.fileService.deleteFiles([files]);
+    }
+  }
+
+  /**
+   * TODO: 添加创建文件的部分
+   *
+   * @param {string} path
+   * @param {('file'|'folder')} type
+   * @returns
+   * @memberof FileTraversalController
+   */
+  @Put()
+  public async createFile(@Body('path') path: string, @Body('type') type: 'file'|'folder') {
+    if(type === 'folder') {
+      return await this.fileService.createFolder(path);
+    }
+    // TODO
+  }
+
+  @Put('copy')
+  public async copyFiles(@Body('sources') sources: Array<string>, @Body('target') target: string) {
+    console.log({ sources, target });
+    return await this.fileService.copyFiles(sources, target);
   }
 
 }
--- src/modules/file-traversal/services/file/file.service.ts
+++ src/modules/file-traversal/services/file/file.service.ts
@@ -1,12 +1,17 @@
+import trash from 'trash';
 import { promisify } from 'util';
 import { FileCarte } from '../../models/public_api';
 import { basename } from 'path';
 import { Injectable } from '@nestjs/common';
-import { stat, lstat, readlink, readdir } from 'fs';
+import { stat, lstat, readlink, readdir, mkdir, exists, copyFile } from 'fs';
+import { async } from 'rxjs/internal/scheduler/async';
 
 const promisifyStat = promisify(stat);
+const promisifyMkdir = promisify(mkdir);
 const promisifyLstat = promisify(lstat);
+const promisifyExists = promisify(exists);
 const promisifyReaddir = promisify(readdir);
+const promisifyCopyFile = promisify(copyFile);
 const promisifyReadlink = promisify(readlink);
 
 @Injectable()
@@ -43,6 +48,27 @@ export class FileService {
       files = files.filter(file => !file.startsWith('.'));
     }
     return files;
+  }
+
+  public async deleteFiles(files: Array<string>) {
+    return trash(files);
+    // return Promise.all(files.map(async (file) => {
+    //   return promisifyUnlink(file)
+    //     .then(() => file);
+    // }));
+  }
+
+  public async createFolder(path: string) {
+    if(await promisifyExists(path)) {
+      throw new Error('file exists');
+    }
+    return await promisifyMkdir(path)
+  }
+
+  public async copyFiles(sources: Array<string>, target: string) {
+    return await Promise.all(sources.map(async (source) => {
+      return await promisifyCopyFile(source, `${ target }/${ basename(source) }`);
+    }));
   }
 
 }
