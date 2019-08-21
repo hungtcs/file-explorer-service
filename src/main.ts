2be58c57fc45367168635b3626af07675711300e
鸿则 <hungtcs@163.com>
2019-08-21 10:42:40 +0800

    feat: add protection security middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
+import csurf from 'csurf';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  app.use(csurf({ cookie: true }));
+  app.use(helmet());
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
+
   await app.listen(4000);
 }
 
